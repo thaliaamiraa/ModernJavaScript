6d31ddf27af9b468c392d677e7af3407fe402c34
thaliaamiraa <thalia02amira@gmail.com>
2022-02-16 12:18:39 +0700

    first commit

---
+++ c_templatestring/template.js
@@ -0,0 +1,11 @@
+let fname = 'Cristian';
+let lname = 'Ronaldo';
+let age = prompt("Masukkan umur Cristian Ronaldo!");
+
+//Cara lama
+// let result = fname + ' ' + age + 'years old';
+// alert (result);
+
+//Memakai Template string
+let result =`${fname} ${lname} is ${age} years old`;
+alert (result);
